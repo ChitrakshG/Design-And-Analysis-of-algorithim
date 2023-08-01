145321d6560d0075e2540907767fbf340781e8ed
Chitraksh Gupta <95366435+ChitrakshGupta@users.noreply.github.com>
2023-08-01 11:29:00 +0530

    Update DepthFirstSearch.java

--- DepthFirstSearch.java
+++ DepthFirstSearch.java
@@ -18,6 +18,28 @@ public class DepthFirstSearch {
             }
         }
     }
+    // other method better way
+    class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj_list) {
+        int vis[]=new int[V];
+        
+         ArrayList<Integer> ar=new  ArrayList<>();
+          dfs(0,adj_list,ar,vis);
+          return ar;
+         
+    }
+    public static void dfs(int v, ArrayList<ArrayList<Integer>> adj_list,  ArrayList<Integer> ar,int vis[]){
+        vis[v]=1;
+        ar.add(v);
+        for(int neighbour : adj_list.get(v)){
+            if(vis[neighbour]!=1){
+                dfs(neighbour,adj_list,ar,vis);
+            }
+        }
+    }
+}
+    // end
     public static void main(String[] args) {
         int no_of_vertices =5;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
